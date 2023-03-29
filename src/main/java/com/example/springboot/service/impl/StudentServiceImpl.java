c8a651934f18a38224e49a269f8f964781cc37cd
selray <124846896+selray@users.noreply.github.com>
2023-03-29 21:34:37 +0800

    利用student登录2

--- src/main/java/com/example/springboot/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/springboot/service/impl/StudentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.springboot.service.impl;
 
+import cn.hutool.log.Log;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.springboot.controller.dto.UserDTO;
 import com.example.springboot.entity.Student;
@@ -19,13 +20,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> implements IStudentService {
+
+    private static final Log LOG = Log.get();
     @Override
     public boolean login(UserDTO userDTO) {
         //去数据库查询的实体类,查找是否有响应数据
         QueryWrapper<Student> queryWrapper= new QueryWrapper<>();
         queryWrapper.eq("logn",userDTO.getUsername());
         queryWrapper.eq("pswd",userDTO.getPassword());
-        Student one = getOne(queryWrapper);
-        return one != null;
+        try{
+            Student one = getOne(queryWrapper);
+            return one != null;
+        }catch (Exception e){
+            LOG.error(e);
+            return false;
+        }
+
     }
 }
