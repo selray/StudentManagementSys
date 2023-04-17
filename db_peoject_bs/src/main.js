0e47d593ab5da5295d579f1be3ac67c1ec40c8c2
selray <124846896+selray@users.noreply.github.com>
2023-04-17 20:24:30 +0800

    更新了登录界面的显示

--- db_peoject_bs/src/main.js
+++ db_peoject_bs/src/main.js
@@ -6,11 +6,12 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/global.css'
 import request from "@/utls/request";
+import VueParticles from 'vue-particles'
 
 
 Vue.config.productionTip = false
 Vue.use(ElementUI, {size: "mini"});
-
+Vue.use(VueParticles)
 Vue.prototype.request = request
 
 new Vue({
--- db_peoject_bs/src/views/Login.vue
+++ db_peoject_bs/src/views/Login.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="wrapper">
-      <div style="margin: 200px auto; background-color: #fff;width: 350px;height: 300px;padding: 20px;border-radius:10px ">
+
+    <div class="login_container">
+      <div class="login_box">
+        <div class="login_form">
+      <div style="margin: 20px auto; width: 350px;height: 300px;padding: 20px;border-radius:10px ">
         <div style="margin: 20px 0; text-align: center;font-size: 24px"><b>登 录</b></div>
         <el-form :model="user" :rules="rules" ref="userForm">
           <el-form-item prop="username">
@@ -10,12 +13,30 @@
             <el-input size="medium" style="margin: 10px 0" prefix-icon="el-icon-lock" show-password v-model="user.password"></el-input>
           </el-form-item>
           <div style="margin: 10px 0;text-align: center">
-            <el-button type="primary" size="small" autocomplete="off" @click="login">登录</el-button>
-            <el-button type="warning" size="small" autocomplete="off" @click="$router.push('/register')">注册</el-button>
+            <el-button type="primary" size="medium" autocomplete="off" @click="login">登录</el-button>
+            <el-button type="warning" size="medium" autocomplete="off" @click="$router.push('/register')">注册</el-button>
           </div>
         </el-form>
-
+      </div>
         </div>
+        </div>
+      <vue-particles
+          color="#DCDCDC"
+          :particleOpacity="0.7"
+          :particlesNumber="60"
+          shapeType="circle"
+          :particleSize="6"
+          linesColor="#DCDCDC"
+          :linesWidth="1"
+          :lineLinked="true"
+          :lineOpacity="0.4"
+          :linesDistance="150"
+          :moveSpeed="3"
+          :hoverEffect="true"
+          hoverMode="grab"
+          :clickEffect="true"
+          clickMode="push">
+      </vue-particles>
       </div>
 
 </template>
@@ -85,10 +106,39 @@ export default {
 </script>
 
 <style>
+.login_container {
+  /*background-image: linear-gradient(-180deg, #1a1454 0%, #0e81a5 100%);*/
+  background-image: url('~@/assets/images/BackGround2.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  overflow: hidden;
+  height: 100vh;
+}
   .wrapper{
     height: 100vh;
     background-image: linear-gradient(to bottom right,#FC466B , #3F5EFB);
     overflow: hidden;
   }
-
+.login_box {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 10px 10px 20px 0px rgba(0, 0, 0, 0.7);
+  background-color: rgba(255, 255, 255, 0.3);
+}
+.login_form{
+  /*background-color: #fff;*/
+  /*width: 600px;*/
+  /*height: 320px;*/
+  /*padding: 20px;*/
+  /*border-radius: 10px;*/
+  /*position: absolute;*/
+  /*background-color: rgba(0, 0, 0, 0.3);*/
+  /*left: 50%;*/
+  /*transform: translate(-50%, -50%);*/
+  /*z-index: 2;*/
+  /*box-shadow: 10px 10px 20px 0px rgba(0, 0, 0, 0.7);*/
+  /*text-align: center;*/
+}
 </style>
